122ecabc94ad97482ac722df4752f6ccbe4fdec5
Yanis <yanis.baouche751@gmail.com>
2023-07-10 14:51:20 +0200

    Bug fix, coordinate now update at each get request

--- orbiteur/views.py
+++ orbiteur/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 from .models import Orbiteur
 from .serializers import OrbiteurSerializer
+import requests
 
 
 class OrbiteurViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
@@ -8,3 +10,28 @@ class OrbiteurViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Cre
     queryset = Orbiteur.objects.all()
     serializer_class = OrbiteurSerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        api_key = "3NT2FE-AUZUVS-3LFRV9-52CO"
+
+        try:
+            response = requests.get(
+                f'https://api.n2yo.com/rest/v1/satellite/positions/{instance.norad_id}/41.702/-76.014/0/2&apiKey={api_key}')
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return Response(serializer.data)
+
+        position_data = response.json()
+        print(position_data)
+
+        if 'positions' in position_data and len(position_data['positions']) > 0:
+            position = position_data['positions'][0]
+            instance.latitude = position['satlatitude']
+            instance.longitude = position['satlongitude']
+            print("LATITUDE,LONGITUDE: ", instance.latitude, instance.longitude)
+            instance.save()
+            serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
